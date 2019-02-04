0782062fa0393f49ed263dd51f50ed878c8cb2e7
ryanisnan <ryanisnan@gmail.com>
2019-02-03 16:27:22 -0800

    add move admin

--- src/sudoku/sudoku/admin.py
+++ src/sudoku/sudoku/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Game
+from .models import Move
 
+
+class MoveAdmin(admin.ModelAdmin):
+    list_display = ('id', 'game', 'x', 'y', 'value')
+    list_filter = ('game',)
+
+
+admin.site.register(Move, MoveAdmin)
 admin.site.register(Game)
