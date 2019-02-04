e6542a1e7fcf3c7e7e6fad97a334b195d3d3bb91
ryanisnan <ryanisnan@gmail.com>
2019-02-03 17:19:15 -0800

    add pretty output

--- src/sudoku/sudoku/api/v1/game.py
+++ src/sudoku/sudoku/api/v1/game.py
@@ -1,5 +1,6 @@
 from ...models import Game
 from django.http import Http404
+from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -31,4 +32,23 @@ class GameAPIDetailViewV1(APIView):
             ]
         }
         '''
+        rendered_game = game.tiles
+
+        if request.GET.get('pretty', False):
+            output = '-' + '---' * 9 + '---\n'
+            for i, row in enumerate(rendered_game):
+                output += '|'
+                for j, tile in enumerate(row):
+                    output += ' %s ' % tile if tile >= 0 else ' '
+                    if j in [2, 5]:
+                        output += '|'
+                output += '|\n'
+
+                # Separator between every 3 rows
+                if i in [2, 5]:
+                    output += '|' + '---' * 9 + '--|\n'
+
+            output += '-' + '---' * 9 + '---\n'
+            return HttpResponse(output, status=status.HTTP_200_OK, content_type='text/plain')
+
         return Response(game.tiles, status=status.HTTP_200_OK)
